0a3efc1a8597e880bb27c967e1ca8bbc21123c05
deconsctruction.events <sanimw3@gmail.com>
2022-03-24 14:43:32 +0300

    fixed navbar

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-$(function () {});
+$(function () {
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  var currentScrollPos = window.pageYOffset;
-
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.position = "fixed";
-    document.getElementById("navbar").style.opacity = "1";
-  } else {
-    document.getElementById("navbar").style.position = "absolute";
-    document.getElementById("navbar").style.opacity = "0";
-  }
-  prevScrollpos = currentScrollPos;
-};
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.position = "fixed";
+      document.getElementById("navbar").style.opacity = "1";
+    } else {
+      document.getElementById("navbar").style.position = "absolute";
+      document.getElementById("navbar").style.opacity = "0";
+    }
+    prevScrollpos = currentScrollPos;
+  };
+});
