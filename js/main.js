06aa857e908a4be9ded76bfdddf822f43f0d7da3
deconsctruction.events <sanimw3@gmail.com>
2022-03-24 14:10:06 +0300

    hidden navbar

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function () {});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.position = "fixed";
+    document.getElementById("navbar").style.opacity = "1";
+  } else {
+    document.getElementById("navbar").style.position = "absolute";
+    document.getElementById("navbar").style.opacity = "0";
+  }
+  prevScrollpos = currentScrollPos;
+};
